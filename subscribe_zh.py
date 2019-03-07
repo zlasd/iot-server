d9e421a9db17eb7d4ef743b5c3a184a8534810b4
HuaZhanggh <39206661+HuaZhanggh@users.noreply.github.com>
2019-03-07 21:11:20 +0800

    Update subscribe_zh.py

--- subscribe_zh.py
+++ subscribe_zh.py
@@ -1,6 +1,17 @@
 import paho.mqtt.client as mqtt
 import base64
 import json
+
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+import config
+from models import Base, Device, Alert
+
+
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -11,6 +22,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    #get the alert info
     img_jpg = 'test1_1.jpg'
     json_data=msg.payload
     raw_data=json.loads(str(json_data, encoding="utf-8"))
@@ -19,6 +31,24 @@ def on_message(client, userdata, msg):
     imgfile = open("/home/zhanghua/Downloads/" + img_jpg, 'wb')
     imgfile.write(img_data)
     imgfile.close()
+
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    engine = create_engine(app.config['DB_CONNECT_STRING'], echo=True)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    DB_Session = sessionmaker(bind=engine)
+    session = DB_Session()
+    #Base.metadata.create_all(engine)
+    device = Device(name='camera0',type=raw_data["type"],addr=raw_data["address"],passwd=raw_data["passwd"])
+    alert=Alert(deviceID=device.ID,personNo=raw_data["personNo"],confidence=raw_data["confidence"])
+    session.add(device)
+    session.add(alert)
+    session.commit()
+    session.close()
+
+
 
 
 client = mqtt.Client()
